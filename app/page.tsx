143a646181875201a67874ab21409f1b48addac1
Mohamed Sharif <mas222@ic.ac.uk>
2025-01-20 12:12:32 +0000

    feature: added homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-
+import Navbar from "@/app/components/Navbar";
+import Hero from "@/app/components/Hero";
+import Footer from "@/app/components/Footer";
 export default function Home() {
   return (
-    <div>
-      Homepage
-    </div>
+    <main className="h-screen overflow-y-auto">
+      <section className = "h-screen">
+        <Navbar />
+        <Hero />
+      </section>
+      <Footer />
+    </main>
   );
 }
