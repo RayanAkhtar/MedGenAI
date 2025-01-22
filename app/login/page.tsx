82d3233f8c7a13b91e5675658df47ee392d3607f
Mohamed Sharif <44378587+MSH4R1F@users.noreply.github.com>
2025-01-22 12:06:00 +0000

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -48,7 +48,7 @@ export default function Login() {
     // Redirect if already logged in
     if (user) {
         router.push('/dashboard')
-        return null // Return null to prevent rendering the login form
+        return null
     }
 
     return (
