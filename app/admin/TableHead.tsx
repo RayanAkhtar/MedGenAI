dd931d47ff699f64d47d7add7fe13087c6d431d4
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2025-03-01 21:07:27 +0000

    debug: resolving errors to get npm run build to work

--- app/admin/TableHead.tsx
+++ app/admin/TableHead.tsx
@@ -7,7 +7,7 @@ interface TableHeadProps {
 
 const TableHead: React.FC<TableHeadProps> = ({ columns }) => {
   return (
-    <thead className="sticky top-0 z-30 bg-blue-500 shadow-md"> {/* Adjust top to 0 and z-index */}
+    <thead className="sticky top-0 z-30 bg-blue-500 shadow-md">
       <tr className="text-white">
         {columns.map((col) => (
           <th key={col} className="px-6 py-4 text-left bg-blue-500">
